a63908dd2123958974dcddacedf8c327ce8b1f82
govardhan reddy <govardahn@gmail.com>
2025-12-20 17:53:55 +0530

    App config SB

--- src/main/java/com/springbootdcoker/ProductController.java
+++ src/main/java/com/springbootdcoker/ProductController.java
@@ -1,5 +1,8 @@
 package com.springbootdcoker;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,11 +13,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ProductController {
 
+	private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
+
+
+	@Autowired
+	private MyProperties properties;
+	
+	
+	@Autowired
+	private org.springframework.core.env.Environment env;
+
+	@GetMapping("/debug")
+	public String debug() {
+	    return "config.message = " + env.getProperty("config.message");
+	}
+
+	@GetMapping("/getDbDetails")
+	public String getMessage() {
+		logger.info("db detailsss {}", properties.getMessage());
+		return "Message: " + properties.getMessage();
+	}
+
 	@GetMapping("/status")
 	public ResponseEntity<String> getStatus() {
-
-		return new ResponseEntity("Deploying in Azure through git - Version3", HttpStatus.OK);
-
+		return new ResponseEntity<>("Deploying in Azure through git - Version3", HttpStatus.OK);
 	}
 
 }
--- src/main/java/com/springbootdcoker/SpringBootDcokerApplication.java
+++ src/main/java/com/springbootdcoker/SpringBootDcokerApplication.java
@@ -2,8 +2,10 @@ package com.springbootdcoker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(MyProperties.class)
 public class SpringBootDcokerApplication {
 
 	public static void main(String[] args) {
